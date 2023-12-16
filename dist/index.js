2140c0ee8b5754462b2076f6b9209b55774f68da
Randy Huls <randyhuls@hotmail.com>
2023-12-16 15:29:10 +0100

    test #10

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
-export * as core from './src/core';
-export * as types from './src/core/types';
-export * as io from './src/io';
-export * as events from './src/events';
+import * as core_1 from './src/core';
+export { core_1 as core };
+import * as types_1 from './src/core/types';
+export { types_1 as types };
+import * as io_1 from './src/io';
+export { io_1 as io };
+import * as events_1 from './src/events';
+export { events_1 as events };
 //# sourceMappingURL=index.js.map
